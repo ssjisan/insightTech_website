e26160bec3e5176a33110ae0d10d01c2a71ffe3e
ssjisan <sakibfset13@gmail.com>
2024-01-03 22:11:42 +0600

    typography & color added

---
+++ src/Components/Theme/overrides.jsx
@@ -0,0 +1,91 @@
+import { alpha } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+export function overrides(theme) {
+    return {
+        MuiCssBaseline: {
+            styleOverrides: {
+                '*': {
+                    boxSizing: 'border-box',
+                },
+                '*::-webkit-scrollbar-thumb': {
+                    backgroundColor: 'red',
+                    borderRadius: '10px',
+                },
+                '*::-webkit-scrollbar-thumb:hover': {
+                    backgroundColor: '#000',
+                }
+            },
+            html: {
+                margin: 0,
+                padding: 0,
+                width: '100%',
+                height: '100%',
+                WebkitOverflowScrolling: 'touch',
+            },
+            body: {
+                margin: 0,
+                padding: 0,
+                width: '100%',
+                height: '100%',
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    "&:hover": {
+                        boxShadow: "none",
+                    },
+                },
+                containedPrimary: {
+                    backgroundColor: theme.palette.primary,
+                    color: "white",
+                    borderRadius: "8px",
+                    boxShadow: "none",
+                    "&:hover": {
+                        boxShadow: "0px 8px 16px 0px rgba(0, 174, 96, 0.24)",
+                        backgroundColor: theme.palette.primary.dark,
+                    },
+                },
+                ghostPrimary: {
+                    backgroundColor: theme.palette.primary.light,
+                    color: theme.palette.primary.main,
+                    border: "1px dashed",
+                    boxShadow: "none",
+                    "&:hover": {
+                        boxShadow: "none",
+                        backgroundColor: alpha(theme.palette.primary.main, 0.16),
+                    },
+                },
+                outlinedPrimary: {
+                    color: theme.palette.primary.main,
+                    border: "1px solid theme.palette.primary.main",
+                    boxShadow: "none",
+                    "&:hover": {
+                        boxShadow: "none",
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                },
+                sizeLarge: {
+                    minHeight: 48,
+                },
+                sizeMedium: {
+                    minHeight: 40,
+                },
+                sizeSmall: {
+                    minHeight: 32,
+                },
+            },
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    color: theme.palette.grey[900],
+                    borderRadius: "8px",
+                    borderColor: theme.palette.grey[500],
+                },
+            },
+        },
+    };
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Box, Button, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+        <Typography variant="h2">Propelling your business to new heights</Typography>
+        <Button variant="contained" color="primary" size="large">large</Button>
+        <Button variant="contained" color="primary" size="medium">medium</Button>
+        <Button variant="contained" color="primary" size="small">small</Button>
+    </Box>
+  )
+}
